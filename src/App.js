4f3e61d06aeabbefcc4ae014d4a0d42379f15780
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-10-21 11:22:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Articles from "./components/Articles";
+
+export default function App() {
+  return (
+    <div className="d-flex flex-row justify-content-center align-items-center p-20">
+      <Articles />
+    </div>
+  );
+}
---
+++ src/components/Articles.js
@@ -0,0 +1,22 @@
+function Articles() {
+  return (
+    <div style={{ width: "700px" }}>
+      <h1 className="mb-20">Liste des articles</h1>
+      <div className="card p-20">
+        <h2 className="mb-10">Titre de l'article</h2>
+        <p>
+          Excepteur laboris excepteur do ad reprehenderit dolor do anim irure.
+          Dolore cupidatat ad irure sunt do excepteur tempor qui mollit eu culpa
+          Lorem. Occaecat culpa laboris in sunt nisi veniam. Mollit ipsum et
+          incididunt pariatur mollit Lorem id aliqua sit elit ex. Veniam
+          adipisicing consequat amet enim. Minim labore aliqua voluptate ipsum
+          mollit occaecat anim deserunt commodo dolore officia proident.
+          Occaecat ex dolore quis dolor aute qui ipsum Lorem ullamco anim
+          incididunt ea laboris dolore.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Articles;
