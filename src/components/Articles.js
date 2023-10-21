8f925dbcbc63292947a4a54cf800a60a3df2d72b
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-10-21 17:55:00 +0200

    method filter

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -5,25 +5,30 @@ function Articles({ displayArticle }) {
     {
       title: "Un titre",
       content: "Un contenu",
+      published: true,
     },
     {
       title: "Un titre 2",
       content: "Un contenu 2",
+      published: false,
     },
     {
       title: "Un titre 3",
       content: "Un contenu 3",
+      published: true,
     },
   ];
   return (
     <div style={{ width: "700px" }}>
       <h1 className="mb-20">Liste des articles</h1>
-      {articles.map((article) => (
-        <div className="mb-20">
-          <h2>{article.title}</h2>
-          <p>{article.content}</p>
-        </div>
-      ))}
+      {articles
+        .filter((article) => article.published)
+        .map((article) => (
+          <div className="mb-20">
+            <h2>{article.title}</h2>
+            <p>{article.content}</p>
+          </div>
+        ))}
     </div>
   );
 }
