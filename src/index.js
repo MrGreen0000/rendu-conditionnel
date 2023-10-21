13148182c726e99d5b4651bca37035c765dc9e37
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-10-21 15:41:46 +0200

    condition

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./assets/styles/index.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-reportWebVitals();
