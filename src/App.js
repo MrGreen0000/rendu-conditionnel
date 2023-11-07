98a850a7e31bc00858395ca5474a368fcd96cc72
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-11-07 11:24:46 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { useState } from "react";
 
 export default function App() {
-  console.log("render");
-
+  console.log("render app");
   const [count, setCount] = useState(0);
   const [showCount, setShowCount] = useState(true);
 
   function handleClick() {
     setCount(count + 1);
+    setShowCount(!showCount);
   }
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center p-20">
-      <button onClick={() => setShowCount(!showCount)}>showCount</button>
       <button onClick={handleClick} className="mb-20">
         Submit {showCount && <span>{count}</span>}
       </button>
---
+++ src/Compteur.js
@@ -0,0 +1,6 @@
+function Compteur({ count }) {
+  console.log("render compteur");
+  return <h1>{count}</h1>;
+}
+
+export default Compteur;
