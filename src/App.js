6b9f57196d91c8238db56d84db6941d90541da52
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-11-07 08:22:58 +0100

    handler event

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import Articles from "./components/Articles";
-
 export default function App() {
+  function handleClick(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    console.log("click ! : ", e);
+  }
+
+  function handleInput(e) {
+    console.log("input : ", e);
+  }
+  function handleFocus(e) {
+    console.log("focus : ", e);
+  }
+
+  function handleClickDiv(e) {
+    console.log("div click: ", e);
+  }
+
   return (
-    <div className="d-flex flex-row justify-content-center align-items-center p-20">
-      <Articles />
+    <div
+      onClick={handleClickDiv}
+      className="d-flex flex-column justify-content-center align-items-center p-20"
+    >
+      <form>
+        <button onClick={handleClick} className="mb-30">
+          Submit
+        </button>
+      </form>
+
+      <input onFocus={handleFocus} type="text" onInput={handleInput}></input>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+function Button() {
+  return <button style={{ backgroundColor: "blue" }}>Submit</button>;
+}
+
+export default Button;
