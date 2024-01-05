8488ca5c747be81f2870e83419f18d7ddaa74536
Naji Mouflih <nmouflih@bluesoft-group.com>
2024-01-05 14:37:46 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,81 @@
 import { useState } from "react";
 
 export default function App() {
-  console.log("render app");
-  const [count, setCount] = useState(0);
-  const [showCount, setShowCount] = useState(true);
+  const [user, setUser] = useState({
+    name: "",
+    age: null,
+  });
 
-  function handleClick() {
-    setCount(count + 1);
-    setShowCount(!showCount);
+  const [userList, setUserList] = useState([]);
+
+  function handleClick(e) {
+    e.preventDefault();
+    const newUserList = [...userList, { ...user }];
+    setUserList(newUserList);
+  }
+
+  function handleInput(e) {
+    let value = e.target.value;
+    let name = e.target.name;
+    if (name === "age") {
+      value = +value;
+    }
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  }
+
+  function handleClickDeleteUser(index) {
+    const newUserList = userList.filter((u, i) => i !== index);
+    setUserList(newUserList);
+  }
+
+  function handlClickSort(index) {
+    const newUserList = [...userList];
+    newUserList.sort((a, b) => a.name.localeCompare(b.name));
+    setUserList(newUserList);
   }
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center p-20">
-      <button onClick={handleClick} className="mb-20">
-        Submit {showCount && <span>{count}</span>}
+      <form className="d-flex flex-column card p-20 mb-20">
+        <input
+          onInput={handleInput}
+          className="mb-20"
+          type="text"
+          name="name"
+          placeholder="Nom"
+        />
+        <input
+          onInput={handleInput}
+          className="mb-20"
+          type="number"
+          name="age"
+        />
+        <button onClick={handleClick} className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      <button
+        onClick={handlClickSort}
+        className="mb-20 btn btn-reverse-primary"
+      >
+        trier la liste
       </button>
+      <ul className="d-flex flex-column card p-20">
+        {userList.map((u, index) => (
+          <li key={u.name} className="d-flex flex-row mb-20">
+            <span className="flex-fill mr-5">{u.name}</span>
+            <button
+              onClick={() => handleClickDeleteUser(index)}
+              className="btn btn-primary"
+            >
+              delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
